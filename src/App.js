602032eb37a84ea88c60226aa73a5a87ea3f687d
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-03-29 13:39:22 +0700

    complete login 1:38pm 29/03/2021

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import  Login from './component/login/Login';
 
 function App() {
+    // declare state 
+    const [presentUser, setPresentUser] = useState({
+        id: '',
+        name: '',
+        numberPhone: '',
+        department: 1
+    });
+    
+    // load user from local Storage
+    useEffect(
+        () => {
+            let data = JSON.parse(localStorage.getItem('user'))?
+                JSON.parse(localStorage.getItem('user')):
+                {
+                    id: '',
+                    name: '',
+                    numberPhone: '',
+                    department: 1
+                };
+            setPresentUser({...data})
+        },[]
+    )
+
+    // handle when complete input user
+    var onSetUser = user => {
+        setPresentUser({
+            ...user
+        })
+
+        localStorage.setItem('user',JSON.stringify(user));
+    }
+
+    // return component
+    var componentsUI = () => {
+        if (presentUser.id.trim() === '') {
+            
+            return (
+                <Login 
+                    setUser={onSetUser}
+                />
+            )
+        }else {
+            return (
+                <div></div>
+            )     
+        }
+    }
+
     return (
         <>
-            <Login/>
+        {
+            componentsUI()
+        }
         </>
     );
 }
--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -3,11 +3,31 @@ import './Login.scss';
 import { rooms } from '../../constands/RoomMap';
 import { useState } from 'react';
 
+// create UUID
+function createUUID() {
+    // http://www.ietf.org/rfc/rfc4122.txt
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+    
+    var uuid = s.join("");
+    return uuid;
+}
+
 // code function here
-function Login() {
+function Login(props) {
+    // get props
+    const { setUser } = props;
+
     // delcare state
     const [valueLogin, setValueLogin] = useState(
         {
+            id: '',
             name: '',
             numberPhone: '',
             department: 1
@@ -15,11 +35,23 @@ function Login() {
     )
 
     // handle when change value
-    
+    var onHandleChange = event => {
+        const value = event.target.value;
+        const name= event.target.name;
+
+        setValueLogin({
+            ...valueLogin,
+            [name]: value
+        })
+    }
 
     // handle when submit form 
     var onHandleSubmit = event => {
         event.preventDefault();
+        setUser({
+            ...valueLogin,
+            id: createUUID()
+        })
     }
 
     // return ui room
@@ -43,6 +75,7 @@ function Login() {
                             className='form-control'
                             name='name'
                             value={valueLogin.name}
+                            onChange={onHandleChange}
                         />
                     </label>
                 </div>
@@ -53,6 +86,7 @@ function Login() {
                             className='form-control'
                             name='numberPhone'
                             value={valueLogin.numberPhone}
+                            onChange={onHandleChange}
                         />
                     </label>
                 </div>
@@ -63,6 +97,7 @@ function Login() {
                             className='form-control'
                             name='department'
                             value={valueLogin.department}
+                            onChange={onHandleChange}
                         >
                         {
                             listRoom
